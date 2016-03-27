7d47a274b94800297ee6d1f6ff88857263f014bc
Phong <p7157568@gmail.com>
2016-03-27 17:27:39 +0700

    feat - in PersonActivity.java, cout list contact

--- app/src/main/java/dhbk/android/readandwritesdcard/PersonActivity.java
+++ app/src/main/java/dhbk/android/readandwritesdcard/PersonActivity.java
@@ -2,12 +2,32 @@ package dhbk.android.readandwritesdcard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class PersonActivity extends AppCompatActivity {
+    private DatabaseHandler db = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person);
+        showData();
+    }
+
+    private void showData() {
+        db = new DatabaseHandler(this);
+
+        // read all contact
+        List<Person> persons = db.getAllContacts();
+        LinearLayout layout = (LinearLayout) findViewById(R.id.linearLayout);
+        for (Person p : persons) {
+            TextView txtview = new TextView(this);
+            String log = "Id: " + p.getId() + " ,Name: " + p.getName() + " , Email: " + p.getEmail();
+            txtview.setText(log);
+            layout.addView(txtview);
+        }
     }
 }
